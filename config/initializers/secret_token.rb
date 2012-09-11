b7da9467f7bfdc79ffca75a8d8f6ceebaa513bc3
Diego Araos <daraosn@gmail.com>
2012-09-11 15:17:29 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicmatch::Application.config.secret_token = '1eb1a4b5d00f6451309bd9bac3b7cc67387d938272d801fd4455e4c376ce2e4e93608516d8709717b5496b0f9a557a57e5b73351789d9bc0950a3ce477c7fabc'
