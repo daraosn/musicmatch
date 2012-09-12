d69f0e082c38edbcf9be067a329d8c2735ac8b31
Diego Araos <daraosn@gmail.com>
2012-09-12 11:27:52 +0200

    fix omniauth login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    abort auth.inspect
-    user = User.where(:provider => auth['provider'], 
-                      :facebook_id => auth['facebook_id'].to_s).first || User.create_with_omniauth(auth)
+    user = User.where(:provider => auth['provider'], :facebook_id => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     if user.email.blank?
       redirect_to edit_user_path(user), :alert => "Please enter your email address."
